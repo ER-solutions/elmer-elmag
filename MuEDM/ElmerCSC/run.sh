5ce614d34a85fb58886fd04b91806bc4e7f4a8e6
ftrillaudp <ftrillaudp@gmail.com>
2021-05-17 18:10:59 -0500

    Now it is working without circuit coupling

--- MuEDM/ElmerCSC/run.sh
+++ MuEDM/ElmerCSC/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+##!/bin/bash
 
 if [ "$#" == "0" ]
 then
@@ -16,12 +16,12 @@ echo "Run the coil powering: coil-energization.sif" | tee $LOGFILE
 rm ./RESU/coil_* | tee -a $LOGFILE
 echo "*** ElmerSolver ***"
 ElmerSolver coil-energization.sif | tee -a $LOGFILE
-python ./Python/plot_ramp.py | tee -a $LOGFILE
-eog ./Figures/ramp.png &
+#python ./Python/plot_ramp.py | tee -a $LOGFILE
+#eog ./Figures/ramp.png &
 #echo "Run the coil extraction: coil-extraction.sif" | tee -a $LOGFILE
 #ElmerSolver coil-extraction.sif | tee -a $LOGFILE
 #python ./Python/plot_all.py | tee -a $LOGFILE
 #eog ./Figures/all_current.png &
 #eog ./Figures/all_voltage.png &
 #mv $LOGFILE log/
-#mv $LOGFILE Log/
+mv $LOGFILE Log/
