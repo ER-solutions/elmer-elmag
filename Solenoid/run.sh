c4555fabcdc71d8a0faa471852f7b7ffc5d24c83
ftrillaudp <ftrillaudp@gmail.com>
2021-05-19 11:05:14 -0500

    New case study: Solenoid

---
+++ Solenoid/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$#" == "0" ]
+then
+	echo "Run name not set for logging purposes. You can give it with: ./run.sh run_name"
+	logname=""
+else
+	echo "Run name set to $1."
+	logname="$1-"
+fi
+
+DATE=$(date +%s)
+LOGFILE="$logname$DATE-run.log"
+echo " "
+echo "Run the coil powering: coil-energization.sif" | tee $LOGFILE
+rm ./RESU/coil_* | tee -a $LOGFILE
+echo "*** ElmerSolver ***"
+ElmerSolver coil-energization.sif | tee -a $LOGFILE
+python ./Python/plot_ramp.py | tee -a $LOGFILE
+eog ./Figures/ramp.png &
+#echo "Run the coil extraction: coil-extraction.sif" | tee -a $LOGFILE
+#ElmerSolver coil-extraction.sif | tee -a $LOGFILE
+#python ./Python/plot_all.py | tee -a $LOGFILE
+#eog ./Figures/all_current.png &
+#eog ./Figures/all_voltage.png &
+#mv $LOGFILE log/
+#mv $LOGFILE Log/
